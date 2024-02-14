5b6a10ab8ce74ab6147feacc5336a56c32fec5f9
Wesley rosa <wesleymrosa@gmail.com>
2024-02-14 15:15:46 -0300

    Adicionado o componente principal e um ícone.

--- cidade-front/src/app/app.module.ts
+++ cidade-front/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule
